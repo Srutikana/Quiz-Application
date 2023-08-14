6cb857c3ae8f14992b78100d3496479893a318b6
Srutikana <116999332+Srutikana@users.noreply.github.com>
2023-08-14 11:51:02 +0530

    Final Commit

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "1.Step by step instruction written to solve any problem is called",
+            "2.Object oriented programming method is followed in",
+            "3.Which of the following is not a valid C variable name?",
+            "4.Which of the following is the correct extension of the Python file?",
+            "5.Which of the following is used to define a block of code in Python language?"
+
+
+
+
+    };
+    public static String choices[][]={
+            {"[a] Pseudocode","[b] Algorithm","[c] Assembler","[d] Class"},
+            {"[a] C Programming Language","[b] C++ Programming Language","[c] C# Programming Language","[d] Both [b] and [c]"},
+            {"[a] int number;","[b] float rate;","[c] int variable_count;","[d] int $main;"},
+            {"[a] .python","[b] .pl","[c] .py","[d] .p"},
+            {"[a] Indentation","[b] Key","[c] Brackets","[d] All of the mentioned"}
+
+    };
+    public static String correctAnswers[]={
+            "[b] Algorithm",
+            "[d] Both [b] and [c]",
+            "[c] .py",
+            "[a] Indentation",
+
+    };
+}
